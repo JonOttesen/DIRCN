correct normalization

train.py
@@ -55,7 +55,7 @@ mask_generator = create_mask_for_mask_type(
 transforms = torchvision.transforms.Compose([
     DownsampleFOV(k_size=320, i_size=320, complex_support=True, quadratic=True),
     ApplyMaskColumn(mask=mask_generator),
-    NormalizeKspace(center_fraction=0.08, complex_support=True),
+    NormalizeKspace(center_fraction=0.04, complex_support=True),
     lambda x: x.astype(np.complex64),
     ComplexNumpyToTensor(complex_support=False),
     ])
@@ -63,7 +63,7 @@ transforms = torchvision.transforms.Compose([
 
 truth_transforms = torchvision.transforms.Compose([
     DownsampleFOV(k_size=320, i_size=320, complex_support=True, quadratic=True),
-    # NormalizeKspace(center_fraction=0.08, complex_support=True),
+    NormalizeKspace(center_fraction=0.04, complex_support=True),
     KspaceToImage(norm='ortho', complex_support=True),
     ComplexAbsolute(),  # Better to use numpy complex absolute than fastmri complex absolute
     lambda x: x.astype(np.float32),